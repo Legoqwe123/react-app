143ec84d993d9de329fbda497364be2ec7756e41
Legoqwe <bill-137@mail.ru>
2020-02-03 23:19:35 +0400

    Updates

---
+++ precache-manifest.a2b05ebaf0464b187ac0984d36bf9402.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29c0df325162441263e900d9a7b656df",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "99a404495ee954738bf5",
+    "url": "/react-app/static/css/main.3bff4aaf.chunk.css"
+  },
+  {
+    "revision": "6e442d67906f12d50c05",
+    "url": "/react-app/static/js/2.10cf6b63.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-app/static/js/2.10cf6b63.chunk.js.LICENSE"
+  },
+  {
+    "revision": "99a404495ee954738bf5",
+    "url": "/react-app/static/js/main.9aa8a9e5.chunk.js"
+  },
+  {
+    "revision": "1d9f74d644a28fde73e0",
+    "url": "/react-app/static/js/runtime-main.79327673.js"
+  },
+  {
+    "revision": "dc05a86ce0a8e75c40e28d2d20c628a9",
+    "url": "/react-app/static/media/Group3.dc05a86c.jpg"
+  }
+]);
