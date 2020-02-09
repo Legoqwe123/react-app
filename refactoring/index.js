9da676c86c818986e52a5d21c4fd27752039272f
Legoqwe <bill-137@mail.ru>
2020-02-09 23:29:21 +0400

    Внесены правки

--- refactoring/index.js
+++ refactoring/index.js
@@ -1,42 +1,42 @@
-  
-/*Исходники*/
 
 function func(s, a, b) {
-    if (s.match(/^$/)) {
-      return -1;
+  if (s.match(/^$/)) {
+    return -1;
+  }
+  var i = s.length - 1;
+  var aIndex = -1;
+  var bIndex = -1;
+  while (aIndex == -1 && bIndex == -1 && i > 0) {
+    if (s.substring(i, i + 1) == a) {
+      aIndex = i;
     }
-    var i = s.length - 1;
-    var aIndex = -1;
-    var bIndex = -1;
-    while (aIndex == -1 && bIndex == -1 && i > 0) {
-      if (s.substring(i, i + 1) == a) {
-        aIndex = i;
-      }
-      if (s.substring(i, i + 1) == b) {
-        bIndex = i;
-      }
-      i = i - 1;
+    if (s.substring(i, i + 1) == b) {
+      bIndex = i;
     }
-    if (aIndex != -1) {
-      if (bIndex == -1) {
-        return aIndex;
-      } else {
-        return Math.max(aIndex, bIndex);
-      }
-    }
-    if (bIndex != -1) {
-      return bIndex;
+    i = i - 1;
+  }
+  if (aIndex != -1) {
+    if (bIndex == -1) {
+      return aIndex;
     } else {
-      return -1;
+      return Math.max(aIndex, bIndex);
     }
   }
-  
-  /*После рефакт.*/
-  
-  function func1(str, a, b) {
-    
-    let aIndex = str.lastIndexOf(a);
-    let bIndex = str.lastIndexOf(b);
-  
-    return Math.max(aIndex, bIndex);
+  if (bIndex != -1) {
+    return bIndex;
+  } else {
+    return -1;
   }
+}
+
+/*После рефакт.*/
+
+function func1(s, a, b) {
+  let arrStr = s
+    .split("")
+    .map((item, index) => (index > 0 ? item : (item = -1)));
+  let aIndex = arrStr.lastIndexOf(a);
+  let bIndex = arrStr.lastIndexOf(b);
+
+  return Math.max(aIndex, bIndex);
+}
